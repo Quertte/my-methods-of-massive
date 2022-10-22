dc7d151a7c1680afaf04d8ce389a8a51761ca804
Quertte <a.injen90@gmail.com>
2022-10-22 16:02:13 +0300

    release 2

--- find.js
+++ find.js
@@ -1,13 +1,7 @@
-function firstUnder(numbers, limit){
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] <= limit)
-    return numbers[i]
-  }
+function firstUnde1r(numbers, limit) {
+  return numbers.find((el) => el <= limit);
 }
 
-function startsWith(strings, letter){
-  for (let i = 0; i < strings.length; i++) {
-    if (strings[i][0] === letter)
-    return strings[i]
-  }
+function startsWith(strings, letter) {
+  return strings.find((el) => el[0] === letter);
 }
