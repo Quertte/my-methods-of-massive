e287e2f78edcab43fad29d334d641b014c5238b7
Quertte <a.injen90@gmail.com>
2022-10-22 15:57:36 +0300

    release my methods

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-plusplus': 0,
+    'no-unused-vars': 0,
+  },
+};
--- codeYourMethods.js
+++ codeYourMethods.js
@@ -1,33 +1,60 @@
-
 // Работать это должно вот так:
 // myForEach([1,2,3], (el) => console.log(el));
 // const newArr = myMap([1,2,3], (el) => el * 2);
 
-
-function myForEach() {
-
+function myForEach(arr, callBack) {
+  for (let i = 0; i < arr.length; i++) {
+    callBack(arr[i]);
+  }
 }
 
-function myMap() {
-
+function myMap(arr, callBack) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callBack(arr[i]));
+  }
+  return newArr;
 }
 
-function myFilter() {
-
+function myFilter(arr, callBack) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callBack(arr[i])) { newArr.push(arr[i]); }
+  }
+  return newArr;
 }
 
-function myFind() {
-
+function myFind(arr, callBack) {
+  for (let i = 0; i < arr.length; i++) {
+    if (callBack(arr[i])) {
+      return arr[i];
+    }
+  }
+  return undefined;
 }
 
-function myEvery() {
-
+function myEvery(arr, callBack) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!callBack(arr[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function mySome() {
-
+function mySome(arr, callBack) {
+  for (let i = 0; i < arr.length; i++) {
+    if (callBack(arr[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function myReduce() {
-
+function myReduce(arr, callBack, initialValue) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += callBack(initialValue, arr[i]);
+  }
+  return sum;
 }
